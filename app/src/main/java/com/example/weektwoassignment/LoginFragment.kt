947195d5a846e8cb82ec787fd15a8e68713c553f
aryankey <w0849138@myscc.ca>
2025-10-01 09:54:37 -0400

    Set username in AppViewModel on successful login.

--- app/src/main/java/com/example/weektwoassignment/LoginFragment.kt
+++ app/src/main/java/com/example/weektwoassignment/LoginFragment.kt
@@ -8,8 +8,9 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.weektwoassignment.databinding.FragmentLoginBinding
 import com.google.android.material.snackbar.Snackbar
-
+import androidx.fragment.app.activityViewModels
 class LoginFragment : Fragment() {
+    private val appViewModel: AppViewModel by activityViewModels()
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
@@ -35,6 +36,7 @@ class LoginFragment : Fragment() {
             binding.passwordLayout.error = if (!passOk) "Required" else null
 
             if (userOk && passOk) {
+                appViewModel.setUsername(u)
                 val nav = findNavController()
                 if (nav.currentDestination?.id == R.id.loginFragment) {
                     nav.navigate(R.id.action_loginFragment_to_homeFragment)
